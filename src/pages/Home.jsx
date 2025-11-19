a7bcb84f3a2ca7e894b7e04cdfc873510347338a
Amitoshdeep <amitosh1999@gmail.com>
2025-11-19 12:18:26 +0530

    black bar to hide line in waves

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,50 +35,55 @@ function Home() {
       </div>
       {/* Waves BG */}
 
-      <svg
-        viewBox="0 0 1440 320"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path fill="#000">
-          <animate
-            attributeName="d"
-            dur="6s"
-            repeatCount="indefinite"
-            values="
-            M0,120
-            Q120,160 240,120
-            T480,110
-            T720,160
-            T960,115
-            T1200,150
-            T1440,120
-            V320H0Z;
+      <div className='relative'>
+        {/* Just black bar to hide a lien */}
+        <div className='absolute bg-black h-2/12 bottom-0 left-0 w-full z-10'></div>
+        <svg
+          viewBox="0 0 1440 320"
+          xmlns="http://www.w3.org/2000/svg"
 
-            M0,120
-            Q120,80 240,120
-            T480,160
-            T720,90
-            T960,75
-            T1200,120
-            T1440,80
-            V320H0Z;
+          >
+          <path fill="#000">
+            <animate
+              attributeName="d"
+              dur="6s"
+              repeatCount="indefinite"
+              values="
+              M0,120
+              Q120,160 240,120
+              T480,110
+              T720,160
+              T960,115
+              T1200,150
+              T1440,120
+              V320H0Z;
 
-            M0,120
-            Q120,160 240,120
-            T480,110
-            T720,160
-            T960,115
-            T1200,150
-            T1440,120
-            V320H0Z;
-            "
-          />
-        </path>
-      </svg>
+              M0,120
+              Q120,80 240,120
+              T480,160
+              T720,90
+              T960,75
+              T1200,120
+              T1440,80
+              V320H0Z;
+
+              M0,120
+              Q120,160 240,120
+              T480,110
+              T720,160
+              T960,115
+              T1200,150
+              T1440,120
+              V320H0Z;
+              "
+              />
+          </path>
+        </svg>
+      </div>
 
       {/* Div 2 Skills Section */}
       <div
-      className='min-h-[70vh] bg-black text-white p-4
+      className='min-h-[70vh] bg-black text-white p-4 pt-0
       flex items-center justify-start flex-col gap-10
       '>
 
